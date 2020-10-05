c712ea16f9ee46c08e91cf40e65457ae9d41b9a2
Saboree <teppoblomholm@outlook.com>
2020-10-05 14:13:34 +0300

    Profile add v0.4
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/healthx/Kayttajatiedot1.java
+++ app/src/main/java/com/example/healthx/Kayttajatiedot1.java
@@ -2,15 +2,21 @@ package com.example.healthx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Kayttajatiedot1 extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    EditText name, ika, pituus, paino;
+    String sname, sika, spituus, spaino;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,36 @@ public class Kayttajatiedot1 extends AppCompatActivity {
         setContentView(R.layout.activity_kayttajatiedot1);
 
         Intent intent1 = getIntent();
+    }
+
+    public void Save(View view) {
+        Intent intent2 = new Intent(this, MainActivity.class);
+
+        name = findViewById(R.id.editNimi);
+        ika = findViewById(R.id.editIka);
+        pituus = findViewById(R.id.editPituus);
+        paino = findViewById(R.id.editPaino);
+
+        sname = name.getText().toString();
+        sika = ika.getText().toString();
+        spituus = pituus.getText().toString();
+        spaino = paino.getText().toString();
+
+        /*
+        intent2.putExtra(EXTRA_MESSAGE1, message1);
+        intent2.putExtra(EXTRA_MESSAGE2, message2);
+        intent2.putExtra(EXTRA_MESSAGE3, message3);
+        intent2.putExtra(EXTRA_MESSAGE4, message4);
+         */
+
+        SharedPreferences sharedPreferences = getSharedPreferences("data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("nimi", sname);
+        editor.putString("ika", sika);
+        editor.putString("pituus", spituus);
+        editor.putString("paino", spaino);
+        editor.commit();
+
+        startActivity(intent2);
     }
 }
--- app/src/main/java/com/example/healthx/MainActivity.java
+++ app/src/main/java/com/example/healthx/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.healthx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +21,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Locates the right TextView by id */
+        TextView text1 = findViewById(R.id.M1);
+        TextView text2 = findViewById(R.id.M2);
+        TextView text3 = findViewById(R.id.M3);
+        TextView text4 = findViewById(R.id.M4);
+
+        /* GetIntent to get Strings from form to MainActivity */
+        Intent intent2 = getIntent();
+
+        /* Loads sharedpreference data and updates values */
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("data", Context.MODE_PRIVATE);
+        String name = sharedPreferences.getString("nimi", "");
+        String ika = sharedPreferences.getString("ika", "");
+        String pituus = sharedPreferences.getString("pituus", "");
+        String paino = sharedPreferences.getString("paino", "");
+
+        text1.setText(name);
+        text2.setText(ika);
+        text3.setText(pituus);
+        text4.setText(paino);
+
+
     }
-    public void sendMessage(View view) {
+
+    protected void onPause() {
+        super.onPause();
+    }
+        /* OnClick function to get into add page */
+        public void add(View view) {
         Intent intent = new Intent(this, AddScreen.class);
         startActivity(intent);
     }
-    public void editProfile(View view) {
+        /* OnClick function to get into editprofile page */
+        public void editProfile(View view) {
         Intent intent1 = new Intent(this, Kayttajatiedot1.class);
         startActivity(intent1);
     }
